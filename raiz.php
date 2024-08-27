5d82a3ee1743d22841a070362ba61cffa0fc92f0
Lucas Dias <91563939+LUk3VIT@users.noreply.github.com>
2024-08-27 16:26:11 -0300

    0.1.1
    Método sacar funciona, agora, fazendo o método transferência para outra conta

--- raiz.php
+++ raiz.php
@@ -2,7 +2,7 @@
 
     class Conta{
         public string $nome;
-        public float $saldo = 0; 
+        public float $saldo; 
         
         function __construct($nome,$saldo){
             $this->nome = $nome;
@@ -18,19 +18,37 @@
             $valor = $this->saldo;
 
             $final = $valor - $sacar;
-            $valor = $final;
 
-            return
-            
+            return $this->saldo = $final;
+        }
+
+        function pix($pix, $destino){
+            $saldo = $this->saldo;
+            $destinoReal = $this->nome;
+
+            if ($destino == $destinoReal){
+                echo "Pessoa ", $destinoReal ," é valido!! \n";
+                if ($pix > 0){
+                    echo "pix de", $pix,"enviado \n";
+                    $final = $pix + $saldo;
+
+                    return $this->saldo = $final;
+
+                } else {
+                    echo "Pix tem que ser maior que 0,00 \n";
+                }
+            }else{
+                echo "Pessoa ", $destino, " não é valido \n";
+            }
         }
     }
 
     $conta = new Conta("Lucas",500);
     $conta->verSaldo();
-
-    $conta->sacar(100);
-
+    $conta->pix(1,"Joao");
     $conta->verSaldo();
 
+    $conta = new Conta("Joao",100);
+    $conta->verSaldo();
 
 ?>
